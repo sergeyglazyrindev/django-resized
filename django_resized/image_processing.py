af993b644da64634d356829d65d55c6accbd9225
Sergey Glazyrin <sergey.glazyrin.dev@gmail.com>
2020-09-20 13:58:22 +0200

    updated handler for gif animated images

--- django_resized/image_processing.py
+++ django_resized/image_processing.py
@@ -49,62 +49,101 @@ class DefaultImageProcessingFactory(IImageProcessingFactory):
         return new_content
 
 
-# Wrap on-the-fly thumbnail generator
-def thumbnails(frames, size, centering=None, method=Image.ANTIALIAS):
+def thumbnails(img, size, centering=None, method=Image.ANTIALIAS):
+    last_frame = img.convert('RGBA')
+    p = img.getpalette()
+    mode = analyseImage(img)['mode']
+    all_frames = []
+    frames = ImageSequence.Iterator(img)
     for frame in frames:
-        thumbnail = frame.copy()
+        if not frame.getpalette():
+            frame.putpalette(p)
+        new_frame = Image.new('RGBA', img.size)
+        if mode == 'partial':
+            new_frame.paste(last_frame)
+        new_frame.paste(frame, (0, 0), frame.convert('RGBA'))
+        last_frame = new_frame.copy()
         if centering:
-            thumbnail = ImageOps.fit(
-                thumbnail,
+            new_frame = ImageOps.fit(
+                new_frame,
                 size,
                 method,
                 centering=centering
             )
         else:
-            thumbnail.thumbnail(size, Image.ANTIALIAS)
-        yield thumbnail
+            new_frame = new_frame.thumbnail(size, Image.ANTIALIAS)
+        all_frames.append(new_frame)
+    return all_frames
 
 
-def cropped_thumbnails(frames, size, box, method=Image.ANTIALIAS):
+def cropped_thumbnails(img, box, method=Image.ANTIALIAS):
+    last_frame = img.convert('RGBA')
+    p = img.getpalette()
+    mode = analyseImage(img)['mode']
+    all_frames = []
+    frames = ImageSequence.Iterator(img)
     for frame in frames:
-        thumbnail = frame.copy()
-        thumbnail = thumbnail.crop(
-            box
-        )
-        yield thumbnail
+        if not frame.getpalette():
+            frame.putpalette(p)
+        new_frame = Image.new('RGBA', img.size)
+        if mode == 'partial':
+            new_frame.paste(last_frame)
+        new_frame.paste(frame, (0, 0), frame.convert('RGBA'))
+        last_frame = new_frame.copy()
+        new_frame = new_frame.crop(box)
+        all_frames.append(new_frame)
+    return all_frames
 
 
-class GifImageProcessingFactory(IImageProcessingFactory):
+def analyseImage(im):
+    """
+    Pre-process pass over the image to determine the mode (full or additive).
+    Necessary as assessing single frames isn't reliable. Need to know the mode
+    before processing all frames.
+    """
+    results = {
+        'size': im.size,
+        'mode': 'full',
+    }
+    try:
+        while True:
+            if im.tile:
+                tile = im.tile[0]
+                update_region = tile[1]
+                update_region_dimensions = update_region[2:]
+                if update_region_dimensions != im.size:
+                    results['mode'] = 'partial'
+                    break
+            im.seek(im.tell() + 1)
+    except EOFError:
+        pass
+    return results
+
+
+class AnimatedGifImageProcessingFactory(IImageProcessingFactory):
 
     def crop(self, size, centering, method=Image.ANTIALIAS):
-        frames = ImageSequence.Iterator(self.img)
-        frames = thumbnails(frames, size, centering=centering, method=method)
-        om = next(frames)
-        om.info = self.img.info
+        frames = thumbnails(self.img, size, centering=centering, method=method)
+        om = frames[0]
         new_content = BytesIO()
-        om.save(new_content, format='gif', save_all=True, append_images=list(frames))
-        self.img = Image.frombytes(om.mode, size, new_content.getvalue(), 'raw')
+        om.save(new_content, format='gif', save_all=True, append_images=frames[1:], loop=0, duration=self.img.info.get('duration', 1000))
+        self.img = Image.open(new_content)
         return self.img
 
     def arbitrary_cropping(self, size, box, method=Image.ANTIALIAS):
-        frames = ImageSequence.Iterator(self.img)
-        frames = cropped_thumbnails(frames, size, box, method=method)
-        om = next(frames)
-        om.info = self.img.info
+        frames = cropped_thumbnails(self.img, box, method=method)
+        om = frames[0]
         new_content = BytesIO()
-        frames = list(frames)
-        om.save(new_content, format='gif', save_all=True, append_images=frames)
-        self.img = Image.frombytes(om.mode, size, new_content.getvalue(), 'raw')
+        om.save(new_content, format='gif', save_all=True, append_images=frames[1:], loop=0, duration=self.img.info.get('duration', 1000)) # , save_all=True, append_images=frames
+        self.img = Image.open(new_content)
         return self.img
 
     def make_thumbnail(self, size, method=Image.ANTIALIAS):
-        frames = ImageSequence.Iterator(self.img)
-        frames = thumbnails(frames, size, method=method)
-        om = next(frames)
-        om.info = self.img.info
+        frames = thumbnails(self.img, size, method=method)
+        om = frames[0]
         new_content = BytesIO()
-        om.save(new_content, format='gif', save_all=True, append_images=list(frames))
-        self.img = Image.frombytes(om.mode, size, new_content.getvalue(), 'raw')
+        om.save(new_content, format='gif', save_all=True, append_images=frames[1:], loop=0, duration=self.img.info.get('duration', 1000))
+        self.img = Image.open(new_content)
         return self.img
 
     def save_to_the_buffer(self, compression_format, quality, **img_info):
@@ -112,7 +151,7 @@ class GifImageProcessingFactory(IImageProcessingFactory):
         frames = ImageSequence.Iterator(self.img)
         om = next(frames)
         om.info = self.img.info
-        om.save(new_content, format='gif', save_all=True, append_images=list(frames))
+        om.save(new_content, format='gif', save_all=True, append_images=list(frames), loop=0, duration=self.img.info.get('duration', 1000))
         new_content = ContentFile(new_content.getvalue())
         return new_content
 
@@ -120,8 +159,9 @@ class GifImageProcessingFactory(IImageProcessingFactory):
 def make_factory_for_image_processing(img):
     factory_per_img_format = {
         'default': DefaultImageProcessingFactory,
-        'gif': GifImageProcessingFactory
     }
-    if img.format.lower() not in factory_per_img_format:
-        return factory_per_img_format['default'](img)
-    return factory_per_img_format[img.format.lower()](img)
+    if img.format.lower() == 'gif':
+        frames = ImageSequence.Iterator(img)
+        if len(frames) > 1:
+            return AnimatedGifImageProcessingFactory(img)
+    return factory_per_img_format['default'](img)
